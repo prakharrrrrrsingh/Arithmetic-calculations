39cf62d61d919736799ce874a04b6ad0a0b67b9c
prakharrrrrrsingh <117091733+prakharrrrrrsingh@users.noreply.github.com>
2022-11-03 17:01:15 +0530

    Add files via upload

---
+++ multiplycode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int first_number,second_number,third_number,multiply;
+cout<<"enter three numbers=";
+cin>> first_number >> second_number >> third_number;
+multiply= first_number * second_number * third_number;
+cout<< first_number <<"*" << second_number <<"*" << third_number <<"="<< multiply;
+
+return 0;
+
+}
+
