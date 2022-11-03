3bfa92a338cc6110290dcd49c062fc77f6a8c0e1
prakharrrrrrsingh <117091733+prakharrrrrrsingh@users.noreply.github.com>
2022-11-03 16:55:30 +0530

    Add files via upload

---
+++ division.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int first_number,second_number,third_number,divide;
+    cout<< "enter 3 num-";
+    cin>> first_number>>second_number>>third_number;
+    divide= first_number / second_number / third_number;
+    cout<< first_number <<"/" <<second_number <<"/"<< third_number<<"="<< divide;
+
+return 0;
+
+}
